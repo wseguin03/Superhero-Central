453f4020d2ef9cf75343280ae51052992e45c8dd
Will Seguin <wseguin@uwo.ca>
2023-11-24 09:49:53 -0500

    Initial Frontend, able to make api calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import SuperheroComponent from './components/SuperheroComponent'; // Add this line
 import './App.css';
+import {Fragment} from 'react';
+import SideBarComponent from './components/SideBarComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <SideBarComponent></SideBarComponent>
+        <SuperheroComponent></SuperheroComponent>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ client/src/components/SideBarComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SideBarComponent.css';
+
+function SideBarComponent() {
+    return (
+        <div className="sidebar">
+            <h3>Sidebar</h3>
+            {/* Add more content here */}
+        </div>
+    );
+}
+
+export default SideBarComponent;
